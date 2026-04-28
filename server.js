a3e035fd24d254e81cf9d2abe9efcce4689d0e43
Christian Nagrama Olgina <56226436+christianmark01@users.noreply.github.com>
2026-04-28 22:48:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+app.post('/login', (req, res) => {
 
+    console.log("===== REQUEST RECEIVED =====");
+    console.log("HEADERS:", req.headers);
+    console.log("BODY:", req.body);
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    console.log("USERNAME:", username);
+    console.log("PASSWORD:", password);
+
+    res.json({
+        received: req.body,
+        username: username,
+        password: password
+    });
+});
